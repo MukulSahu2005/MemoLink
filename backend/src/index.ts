a05e4247a379148fe449b2d1fbc5bac2d618f60e
Mukul Sahu <mukulsahu2005@gmail.com>
2026-06-21 02:59:09 +0530

    trying error solving for deployment

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,14 +4,30 @@ import "dotenv/config";
 import connectDB from "./db/index.js";
 import { app } from "./app.js";
 
+// connectDB()
+//   .then(() => {
+//     // 3000 ko hata kar 10000 kar diya (Render ka default safe port)
+//     const port = process.env.PORT || 3000; 
+//     app.listen(port, () => {
+//       console.log(`⚙️  Server is running at port : ${port}`);
+//     });
+//   })
+//   .catch((err) => {
+//     console.error("MongoDB connection failed !!! ", err);
+//   });
+
+// CHECKIING FOR DEPLOYMENT
+
+// 1. SHUTTER SABSE PEHLE KHOLO (Render instantly 'LIVE' mark kar dega)
+app.listen(PORT, () => {
+  console.log(`⚙️  Server is officially LIVE at port: ${PORT}`);
+});
+
+// 2. Database ko aaram se pichhe connect hone do
 connectDB()
   .then(() => {
-    // 3000 ko hata kar 10000 kar diya (Render ka default safe port)
-    const port = process.env.PORT || 3000; 
-    app.listen(port, () => {
-      console.log(`⚙️  Server is running at port : ${port}`);
-    });
+    console.log("🍃 MongoDB Atlas Connected Successfully!");
   })
   .catch((err) => {
-    console.error("MongoDB connection failed !!! ", err);
+    console.error("❌ MongoDB Connection FATAL ERROR: ", err);
   });
